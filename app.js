628a860a0acc38095481ce05308413e402109777
Hannah Wynn Jones <hannahwynnjones@hotmail.co.uk>
2018-01-01 14:15:53 +0000

    refactoring error handlerand custom responses

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
+const errorHandler = require('./lib/errorHandler');
+const customResponses = require('./lib/customResponses');
 
 var path = require('path');
 var favicon = require('serve-favicon');
 mongoose.connect('mongodb://localhost/book-list-mern-crud', { useMongoClient: true, promiseLibrary: require('bluebird') })
   .then(() =>  console.log('connection succesful WHOOPPEE'))
   .catch((err) => console.error(err));
-
 
 const routes = require('./config/routes');
 var app = express();
@@ -19,25 +20,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({'extended':'false'}));
 app.use(express.static(path.join(__dirname, 'build')));
 
+app.use(customResponses);
 app.use('/api', routes);
 app.get('/*', (req, res) => res.sendFile(`${__dirname}/public/index.html`));
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.use(errorHandler);
 
 module.exports = app;
