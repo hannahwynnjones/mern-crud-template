ad8b28796b2b657df9f4ed725fce7d1bd11c9f05
Hannah Wynn Jones <hannahwynnjones@hotmail.co.uk>
2018-01-01 14:47:16 +0000

    changed mongo db

--- app.js
+++ app.js
@@ -8,14 +8,19 @@ const customResponses = require('./lib/customResponses');
 
 var path = require('path');
 var favicon = require('serve-favicon');
-mongoose.connect('mongodb://localhost/book-list-mern-crud', { useMongoClient: true, promiseLibrary: require('bluebird') })
-  .then(() =>  console.log('connection succesful WHOOPPEE'))
-  .catch((err) => console.error(err));
+
+const { port, env, dbURI } = require('./config/environment');
+
+// mongoose.connect('mongodb://localhost/book-list-mern-crud', { useMongoClient: true, promiseLibrary: require('bluebird') })
+//   .then(() =>  console.log('connection succesful WHOOPPEE'))
+//   .catch((err) => console.error(err));
 
 const routes = require('./config/routes');
 var app = express();
 
 app.use(morgan('dev'));
+mongoose.connect(dbURI);
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({'extended':'false'}));
 app.use(express.static(path.join(__dirname, 'build')));
@@ -26,5 +31,6 @@ app.get('/*', (req, res) => res.sendFile(`${__dirname}/public/index.html`));
 
 
 app.use(errorHandler);
+app.listen(port, () => console.log(`Express is listening on port ${port}`));
 
 module.exports = app;
